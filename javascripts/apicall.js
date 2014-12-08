fff9a64fd8ae1944a4455a207489c815295863da
Shrutika Dasgupta <sd2841@columbia.edu>
2014-12-08 05:14:27 -0500

    UI Modified

--- javascripts/apicall.js
+++ javascripts/apicall.js
@@ -129,14 +129,203 @@ function processResults(data) {
       $("#where").append('');
 
       for(i = 0; i<num; i++){
-        var idNum = offset+i+1;
-        $("#where").append('</br>');
-        $("#where").append(" Event#"+idNum+"  ");
-        $("#where").append('<div style = "margin-left: 80%" title = "'+ data.results[i].event_name.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"")+ '" id = "pinDiv'+ idNum +'" class="pin-ctr" name = "' +data.results[i].event_detail_url +'"><ul><li><a href="javascript:pinEvent('+idNum+')"><span class="glyphicon glyphicon-pushpin"></span></a></li></ul></div>');
+        var unique_id = (offset+i+1);
+        var allResults = data.results[i];
+        var bourough = allResults.bourough;
+        var category = allResults.category;
+        var city = allResults.city;
+        var event_url = allResults.event_detail_url;
+        var event_id = allResults.event_id;
+        var event_name = allResults.event_name;
+        var cross_street = allResults.cross_street;
+        var event_state = allResults.state;
+        var venue_name = allResults.venue_name;
+        var web_description = allResults.web_description;
+        var telephone = allResults.telephone;
+
+        var corgi_feed_well = document.createElement("div");
+        corgi_feed_well.id = "corgi_feed_well/"+unique_id;
+        corgi_feed_well.className = "corgi_feed_well";
+        document.getElementById("where").appendChild(corgi_feed_well);
+
+        var individual_feed_item = document.createElement("div");
+        individual_feed_item.id = "individual_feed_item/"+unique_id;
+        individual_feed_item.className = "individual_feed_item";
+        document.getElementById(corgi_feed_well.id).appendChild(individual_feed_item);
+
+        var feed_item = document.createElement("div");
+        feed_item.id = "feed_item/"+unique_id;
+        feed_item.className = "feed_item";
+        document.getElementById(individual_feed_item.id).appendChild(feed_item);
+
+        var feed_body = document.createElement("div");
+        feed_body.id = "feed_body/"+unique_id;
+        feed_body.className = "feed_body";
+        document.getElementById(feed_item.id).appendChild(feed_body);
+
+        var row1 = document.createElement("div") ;
+        row1.id = "row1_feed/"+unique_id;
+        row1.className = "row";
+        document.getElementById(feed_body.id).appendChild(row1);
+
+        var feed_profile_pic = document.createElement("div");
+        feed_profile_pic.id = "feed_profile_pic/"+unique_id;
+        feed_profile_pic.className = "feed_profile_pic";
+        document.getElementById(row1.id).appendChild(feed_profile_pic);
+
+        var meta_image = document.createElement("img");
+        meta_image.id = "meta_image/"+unique_id;
+        meta_image.className = "meta_image";
+        meta_image.src = "images/quotes-2.png";
+        document.getElementById(feed_profile_pic.id).appendChild(meta_image);
+
+        var feed_text = document.createElement("div");
+        feed_text.id = "feed_text/"+unique_id;
+        feed_text.className = "feed_text";
+        document.getElementById(row1.id).appendChild(feed_text);
+
+        var text_para = "<h3 class=\"custom-head\">"+event_name+"</h3><p>"+web_description+"</p>";
+        document.getElementById(feed_text.id).innerHTML += text_para;
+
+        var comment_area = document.createElement("div");
+        comment_area.id = "comment_area/"+event_id;
+        comment_area.className = "comment_area";
+        document.getElementById(feed_item.id).appendChild(comment_area);
+
+        var feed_hr1 = document.createElement("hr");
+        feed_hr1.id = "hr1-"+unique_id;
+        feed_hr1.className = "feed_hr";
+        document.getElementById(feed_item.id).appendChild(feed_hr1);
+
+        var bottom_up = document.createElement("div");
+        bottom_up.id = "bottom_up-"+unique_id;
+        bottom_up.className = "bottom_meta";
+        document.getElementById(feed_item.id).appendChild(bottom_up);
+
+        var row_up = document.createElement("div");
+        row_up.id = "row_up-"+unique_id+"-"+event_id;
+        row_up.className = "row user-id-search";
+        document.getElementById(bottom_up.id).appendChild(row_up);
+
+        $("#"+row_up.id).append('Rate this event: <div style="margin: 0px" title = "'+ data.results[i].event_name.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"") + '"><div title = "'+ data.results[i].event_name.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"")+ '" id = "starDiv'+ unique_id +'" class="star-ctr"><ul><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li></ul></div></div>');
+/*
+        var stars = document.createElement("input");
+        stars.id = "input-22-"+unique_id;
+        stars.type = "number";
+        stars.className = "rating";
+        stars.addEventListener('click', function(){
+          console.log("clicked event");
+        });
+        document.getElementById(row_up.id).appendChild(stars);*/
+
+       /* $("#input-22-"+unique_id).rating({'min':0, 'max':5, 'step':1, 'size':'xs'});
+        $('#input-22-'+unique_id).addClass('value-of-rating');*/
+
+        var feed_hr = document.createElement("hr");
+        feed_hr.id = "hr-"+unique_id;
+        feed_hr.className = "feed_hr";
+        document.getElementById(feed_item.id).appendChild(feed_hr);
+
+        var bottom_meta = document.createElement("div");
+        bottom_meta.id = "bottom_meta-"+unique_id;
+        bottom_meta.className = "bottom_meta";
+        document.getElementById(feed_item.id).appendChild(bottom_meta);
+
+        var row2 = document.createElement("div");
+        row2.id = "row2-"+unique_id;
+        row2.className = "row";
+        document.getElementById(bottom_meta.id).appendChild(row2);
+
+        var bottom_left = document.createElement("div");
+        bottom_left.id = "bottom_left-"+unique_id;
+        bottom_left.className = "bottom_left";
+        document.getElementById(row2.id).appendChild(bottom_left);
+
+        var bottom_left_text = "<h4 class=\"icon-adult\">&nbsp;"+event_name+"</h4><h4 class=\"icon-calendar-sign\">&nbsp;"+category+"</h4><h4 class=\"icon-map-marker-alt\">&nbsp;"+venue_name+", "+cross_street+", "+city+", "+event_state+"</h4>";
+
+        document.getElementById(bottom_left.id).innerHTML += bottom_left_text;
+
+        var bottom_right = document.createElement("div");
+        bottom_right.id = "bottom_right-"+unique_id;
+        bottom_right.className = "bottom_right";
+        document.getElementById(row2.id).appendChild(bottom_right);
+
+        var a_bottom_right = document.createElement("a");
+        a_bottom_right.id = "a_bottom_right-"+unique_id;
+        a_bottom_right.href = event_url;
+        a_bottom_right.target = "_blank";
+        var view_art = document.createTextNode("View Event");
+        a_bottom_right.appendChild(view_art);
+        document.getElementById(bottom_right.id).appendChild(a_bottom_right);
+
+        var span_bottom_right = document.createElement("span");
+        span_bottom_right.id = "span_bottom_right-"+unique_id;
+        var span_sym = document.createTextNode("|");
+        span_bottom_right.appendChild(span_sym);
+        document.getElementById(bottom_right.id).appendChild(span_bottom_right);
+
+        /*var show_more_comments = document.createElement("button");
+        show_more_comments.id = "show_more_comments-"+random_comment['userId'];
+        show_more_comments.className = "show_comment_link btn btn-link";
+        show_more_comments.type = "button"
+        show_more_comments.addEventListener('click', function(e){
+          e.preventDefault();
+          $(this).closest('.feed_item').find('.comment_area').slideToggle(700);
           
-        /*var res = (data.results.comments[i].userComments).split('/');
+          var user_array = [];
+          user_array = (this.id).split('-');
+          var userId = user_array[1];
+          console.log(userId);
+          $.ajax({
+            type:'GET',
+            url: 'http://api.nytimes.com/svc/community/v2/comments/user/id/'+userId+'.jsonp?api-key=71b688b6fcaffe9ac59413b1d7de1a0c:1:70151851',
+            cache: false,
+            async: false,
+            dataType: 'jsonp',
+            success: function(data){
+              var objects = data.results.comments;
+              for (var j = 0; j < objects.length; j++){
+                var user_comments = objects[j]['commentBody'];
+                var user_name = objects[j]['display_name'];
+                var user_location = objects[j]['location'];
+                var u_dateTime = new Date(objects[j]['approveDate']*1000);
+                var u_day = u_dateTime.getDate();
+                var u_month = u_dateTime.getMonth();
+                var u_year = u_dateTime.getFullYear();
+
+                var user_date = u_month+"/"+u_day+"/"+u_year;
+
+                var div_other_comm = document.createElement("div");
+                div_other_comm.id = "div_other_comments/"+userId+"/"+j;
+                div_other_comm.className = "div-other-comments";
+                div_other_comm.style.border = '2px solid black';
+                document.getElementById("comment_area/"+userId).appendChild(div_other_comm);
+                var p_text  = "<p class = \"bg-primary text_box_comments\">"+user_comments+"</p>";
+                document.getElementById(div_other_comm.id).innerHTML += p_text;
+                var info = "<p id = \"info-"+unique_id+"-"+j+"\" class =\" bg-danger text_box_comments\"></p>";
+                document.getElementById(div_other_comm.id).innerHTML += info;
+                var all_info = "<h4 class=\"icon-adult\">&nbsp;"+user_name+"</h4><h4 class=\"icon-calendar-sign\">&nbsp;"+user_date+"</h4><h4 class=\"icon-map-marker-alt\">&nbsp;"+user_location+"</h4>";
+                $('p#info-'+unique_id+"-"+j).html(all_info);
+              }
+            },
+            error: function(){
+              console.log("error");
+            }
+          });
+
+        }, false);*/
+        /*var show_more_text = document.createTextNode("View Commentor's Comments");
+
+        show_more_comments.appendChild(show_more_text);
+        document.getElementById(bottom_right.id).appendChild(show_more_comments);*/
+/*
+        $("#where").append('</br>');
+        $("#where").append(" Event#"+(offset+unique_id+1)+"  ");
+        $("#where").append('<div style = "margin-left: 80%" title = "'+ data.results[i].event_name.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"")+ '" id = "pinDiv'+ i +'" class="pin-ctr" name = "' +data.results[i].event_detail_url +'"><ul><li id= "test'+unique_id+'"><a href="#" id= "test'+unique_id+'"><span class="glyphicon glyphicon-pushpin"></span></a></li></ul></div>');
+          
+        var res = (data.results.comments[i].userComments).split('/');
       var last = res[res.length-1].split(".");
-      useridList.push(last[0]);*/
+      useridList.push(last[0]);
 
       
       // console.log('<div id = "starDiv'+ i +'" style="margin: 0px"><div class="star-ctr"><ul><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li><li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li></ul></div></div>')
@@ -148,19 +337,19 @@ function processResults(data) {
       $("#where").append(data.results[i].web_description);
       $("#where").append('</br>');
 
-      /*var aTag = document.createElement('a');
+      var aTag = document.createElement('a');
       aTag.setAttribute('href',data.results.comments[i].articleURL);
       // mydiv.appendChild(aTag);
 
-      $("#where").append(aTag);*/
-      /*$("#where").append('User: '+data.results[i].display_name);
-      $("#where").append('</br>'); */     
+      $("#where").append(aTag);
+      $("#where").append('User: '+data.results[i].display_name);
+      $("#where").append('</br>');    
       $("#where").append('Event url: ');
       $("#where").append('<a href="' + data.results[i].event_detail_url + '" target="_blank">' + data.results[i].event_detail_url + '</a>');
 
       $("#where").append('</br>');
-      /*$("#where").append('Recommended by: '+data.results.recommendationCount);
-      $("#where").append('</br>');*/
+      $("#where").append('Recommended by: '+data.results.recommendationCount);
+      $("#where").append('</br>');
       $("#where").append('Where: ');
       $("#where").append('</br>');
       $("#where").append(data.results[i].venue_name);
@@ -182,9 +371,9 @@ function processResults(data) {
       // console.log(last[0]);
       // $("#where").append('</br>');
 
-      $("#where").append('<div class = "otherComments"></div>');
+      /*$("#where").append('<div class = "otherComments"></div>');*/
 
-      var $me = $('#starDiv'+i);
+      var $me = $('#starDiv'+unique_id);
 
       $bg = $me.children( 'ul' );
       $fg = $bg.clone().addClass( 'star-fg' ).css( 'width', 0 ).appendTo( $me );
@@ -207,7 +396,7 @@ function processResults(data) {
       // document.getElementById("addrwhere").style.color = "black";
       document.getElementById("where").style.color = "black";
       // document.getElementById("where").style.backgroundColor = "#EACCFF";
-      document.getElementById("where").style.backgroundImage="url('Paper_Texture.jpg')";
+      //document.getElementById("where").style.backgroundImage="url('Paper_Texture.jpg')";
       document.getElementById("where").style.textAlign = "left";
       // document.getElementById("addrwhere").style.backgroundColor = "lightGray";
 
